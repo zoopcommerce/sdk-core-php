7a93fa511cd55e70df2a76b6111d7e5f9f9b3ecb
Ganesh Hegde <1ganesh1@facebook.com>
2012-12-07 10:48:53 +0530

    ipn support added - first commit

---
+++ tests/PPIPNMessageTest.php
@@ -0,0 +1,44 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+require_once 'PPIPNMessage.php';
+
+/**
+ * Test class for PPIPNMessage.
+ *
+ */
+class PPIPNMessageTest extends PHPUnit_Framework_TestCase {
+	/**
+	 * @test
+	 */
+	
+	public function passGoodIPN() {
+		
+	}
+	
+	
+	/**
+	 * @test
+	 */
+	
+	public function failOnBadIPN() {
+	
+	}
+	
+	
+	/**
+	 * @test
+	 */
+	
+	public function processIPNWithArrayElements() {
+	
+	}
+	
+	/**
+	 * @test
+	 */
+	
+	public function processIPNWithSpecialCharacters() {
+	
+	}
+	
+}
