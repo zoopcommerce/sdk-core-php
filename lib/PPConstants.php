7b830a6365a0b86be7de5dc80d820c76a60e28f0
Ganesh Hegde <1ganesh1@facebook.com>
2013-03-21 16:03:57 +0530

    constants added

---
+++ lib/PPConstants.php
@@ -0,0 +1,10 @@
+<?php
+define("MERCHANT_SANDBOX_SIGNATURE_ENDPOINT", "https://api-3t.sandbox.paypal.com/2.0");
+define("MERCHANT_SANDBOX_CERT_ENDPOINT", "https://api.sandbox.paypal.com/2.0");
+define("PLATFORM_SANDBOX_ENDPOINT", "https://svcs.sandbox.paypal.com/");
+define("IPN_SANDBOX_ENDPOINT" , "https://sandbox.paypal.com/cgi-bin/webscr");
+
+define("MERCHANT_LIVE_SIGNATURE_ENDPOINT", "https://api-3t.paypal.com/2.0");
+define("MERCHANT_LIVE_CERT_ENDPOINT", "https://api.paypal.com/2.0");
+define("PLATFORM_LIVE_ENDPOINT", "https://svcs.paypal.com/");
+define("IPN_LIVE_ENDPOINT" , "https://ipnpb.paypal.com/cgi-bin/webscr");
