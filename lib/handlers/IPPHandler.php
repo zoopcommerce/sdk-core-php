178827f25f08309191fe14be791887ba28c6b5cb
Ganesh Hegde <1ganesh1@facebook.com>
2013-03-21 16:19:20 +0530

    logger commented for dev

--- lib/handlers/IPPHandler.php
+++ lib/handlers/IPPHandler.php
@@ -5,5 +5,5 @@ interface IPPHandler {
 	 * @param PPHttpConfig $httpConfig
 	 * @param PPRequest $request 
 	 */
-	public function handle($httpConfig, $request);
+	public function handle($httpConfig, $request, $options);
 }
