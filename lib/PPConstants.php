1800a9129be7cb6ade677c55d77c85dc24593d7e
Ganesh Hegde <1ganesh1@facebook.com>
2013-03-25 11:41:12 +0530

    fixed a typo

--- lib/PPConstants.php
+++ lib/PPConstants.php
@@ -1,14 +1,14 @@
 <?php
 class PPConstants
 {
-    const MERCHANT_SANDBOX_SIGNATURE_ENDPOINT = "https://api-3t.sandbox.paypal.com/2.0");
-    const MERCHANT_SANDBOX_CERT_ENDPOINT = "https://api.sandbox.paypal.com/2.0");
-    const PLATFORM_SANDBOX_ENDPOINT = "https://svcs.sandbox.paypal.com/");
-    const IPN_SANDBOX_ENDPOINT = "https://sandbox.paypal.com/cgi-bin/webscr");
+    const MERCHANT_SANDBOX_SIGNATURE_ENDPOINT = "https://api-3t.sandbox.paypal.com/2.0";
+    const MERCHANT_SANDBOX_CERT_ENDPOINT = "https://api.sandbox.paypal.com/2.0";
+    const PLATFORM_SANDBOX_ENDPOINT = "https://svcs.sandbox.paypal.com/";
+    const IPN_SANDBOX_ENDPOINT = "https://sandbox.paypal.com/cgi-bin/webscr";
 
-    const MERCHANT_LIVE_SIGNATURE_ENDPOINT = "https://api-3t.paypal.com/2.0");
-    const MERCHANT_LIVE_CERT_ENDPOINT = "https://api.paypal.com/2.0");
-    const PLATFORM_LIVE_ENDPOINT = "https://svcs.paypal.com/");
-    const IPN_LIVE_ENDPOINT = "https://ipnpb.paypal.com/cgi-bin/webscr");
+    const MERCHANT_LIVE_SIGNATURE_ENDPOINT = "https://api-3t.paypal.com/2.0";
+    const MERCHANT_LIVE_CERT_ENDPOINT = "https://api.paypal.com/2.0";
+    const PLATFORM_LIVE_ENDPOINT = "https://svcs.paypal.com/";
+    const IPN_LIVE_ENDPOINT = "https://ipnpb.paypal.com/cgi-bin/webscr";
 }
 
