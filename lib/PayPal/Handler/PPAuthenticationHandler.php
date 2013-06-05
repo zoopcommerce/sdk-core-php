7605e47809ce918c60a2c4d75171aaf9a738bfb9
Ganesh Hegde <1ganesh1@facebook.com>
2013-06-05 22:01:17 +0530

    adding missing port check

--- lib/PayPal/Handler/PPAuthenticationHandler.php
+++ lib/PayPal/Handler/PPAuthenticationHandler.php
@@ -18,7 +18,7 @@ class PPAuthenticationHandler implements IPPHandler {
 			$thirdPartyAuth = $credential->getThirdPartyAuthorization();
 			if($thirdPartyAuth && $thirdPartyAuth instanceof PPTokenAuthorization) {
 				$authSignature = AuthSignature::generateFullAuthString($credential->getUsername(), $credential->getPassword(), $thirdPartyAuth->getAccessToken(), $thirdPartyAuth->getTokenSecret(), $httpConfig->getMethod(), $httpConfig->getUrl());
-				if($options['port'] == 'PayPalAPI') {
+				if($options['port'] == 'PayPalAPI' || $options['port'] == 'PayPalAPIAA') {
 					$httpConfig->addHeader('X-PP-AUTHORIZATION', $authSignature);
 				}
 				else {
