8292ee1794bba0531e17aab3bbeca740ce525948
Ganesh Hegde <1ganesh1@facebook.com>
2013-08-12 14:52:08 +0530

    correcting spelling

--- lib/PayPal/Handler/GenericSoapHandler.php
+++ lib/PayPal/Handler/GenericSoapHandler.php
@@ -5,20 +5,19 @@ use PayPal\Auth\PPSubjectAuthorization;
 use PayPal\Auth\Oauth\AuthSignature;
 use PayPal\Core\PPConstants;
 use PayPal\Handler\IPPHandler;
-use Urn\Ebay\Api\PayPalAPI\PayPalAPIInterfaceService;
 
 class GenericSoapHandler implements IPPHandler {
 
 	private $namespace;
 	
-	public function __construct($namspace) {
-		$this->namespace = $namspace;
+	public function __construct($namespace) {
+		$this->namespace = $namespace;
 	}
 	
 	public function handle($httpConfig, $request, $options) {
 		$httpConfig->setUrl($options['config']['service.EndPoint']);
 		$httpConfig->addHeader('Content-Type', 'text/xml');
-		$request->addBindingInfo("namespace", "$this->namespace");
+		$request->addBindingInfo("namespace", $this->namespace);
 		if(isset($options['securityHeader']))
 		{
 			$request->addBindingInfo('securityHeader' , $options['securityHeader']->toXMLString());
