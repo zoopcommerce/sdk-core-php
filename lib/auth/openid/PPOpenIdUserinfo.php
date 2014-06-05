f0cf7fae0516bff6b6380c3f48682cf7a13444c1
James R Grinter <jrg@acm.org>
2014-06-05 11:13:01 +0100

    Need full namespace of PPOpenIdAddress, for PPModel magic to work.

--- lib/auth/openid/PPOpenIdUserinfo.php
+++ lib/auth/openid/PPOpenIdUserinfo.php
@@ -279,7 +279,7 @@ class PPOpenIdUserinfo extends PPModel {
 		 
 		/**
 		 * End-User's preferred address.
-		 * @param PPOpenIdAddress $address
+		 * @param PayPal\Auth\Openid\PPOpenIdAddress $address
 		 */
 		 public function setAddress($address) {
 		 	$this->address = $address;
@@ -288,7 +288,7 @@ class PPOpenIdUserinfo extends PPModel {
 		
 		/**
 		 * End-User's preferred address.
-		 * @return PPOpenIdAddress
+		 * @return PayPal\Auth\Openid\PPOpenIdAddress
 		 */
 		 public function getAddress() {
 		 	return $this->address;
